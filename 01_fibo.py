3dde6eadb0dfb94573b91b5954e01212875e6bdc
jinscott88 <jinscott88@gmail.com>
2025-06-23 17:25:34 +0900

    250623

---
+++ 01_fibo.py
@@ -0,0 +1,16 @@
+def fibo(x):
+    # base case
+    if x == 0:
+        return 0
+    if x == 1:
+        return 1
+    
+    # recursive case
+    if x > 1:
+        return fibo(x - 1) + fibo(x - 2)
+    else:
+        return fibo(x + 2) + fibo(x + 1)
+
+if __name__ == "__main__":
+    n = int(input())
+    print(fibo(n))
